8637ca8cc76fbc767557ae850766cbd4863a41ae
Luis Johnatan <hola@lacuevadelterrorpe.com>
2025-10-09 17:36:09 -0500

    fix: fully disable prerendering for /pago/error

--- app/pago/error/page.tsx
+++ app/pago/error/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
+export const revalidate = 0; // 👈 esta línea es la clave final
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
