25ed5108c2cd25b9361d9fa72ae789e375672419
Luis Johnatan <hola@lacuevadelterrorpe.com>
2025-10-09 17:33:59 -0500

    fix: disable prerendering on /pago/revisar page

--- app/pago/error/page.tsx
+++ app/pago/error/page.tsx
@@ -5,8 +5,6 @@ export const dynamic = "force-dynamic";
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export const dynamic = "force-dynamic";
-
 export default function ErrorPagoPage() {
   const router = useRouter();
   const params = useSearchParams();
