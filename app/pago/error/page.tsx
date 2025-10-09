42af75d5fc776338880854a88e95264c15682fdd
Luis Johnatan <hola@lacuevadelterrorpe.com>
2025-10-09 17:38:21 -0500

    fix: remove invalid revalidate from client page

--- app/pago/error/page.tsx
+++ app/pago/error/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0; // 👈 esta línea es la clave final
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
