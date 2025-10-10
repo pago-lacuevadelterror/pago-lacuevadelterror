fc69e8e6e2fda279f491608b0e5404af149a91d2
Luis Johnatan <hola@lacuevadelterrorpe.com>
2025-10-09 20:59:12 -0500

    move main payment page to root route

--- app/pago/credito/page.tsx
+++ app/pago/credito/page.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/navigation";
 import RightPanel from "@/components/RightPanel";
 
 export default function CreditoPage() {
-  const [monto] = useState<number>(150);
+  const [monto] = useState<number>(330);
   const router = useRouter();
   const [form, setForm] = useState({
     tarjeta: "",
